572cc49d8880cee642d284fb475340ecc1362b39
gegehprast <gegeh.prast@gmail.com>
2022-08-31 01:52:07 +0700

    dbConnect

---
+++ lib/dbConnect.ts
@@ -0,0 +1,49 @@
+import mongoose, { Mongoose } from 'mongoose'
+
+type Cached = { conn: null | Mongoose; promise: null | Promise<Mongoose> }
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+    throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env.local'
+    )
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+// @ts-ignore: Unreachable code error
+let cached: Cached = global.mongoose
+
+if (!cached) {
+    // @ts-ignore: Unreachable code error
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts: mongoose.ConnectOptions = {
+            bufferCommands: false,
+            autoIndex: true,
+        }
+
+        cached.promise = mongoose
+            .connect(MONGODB_URI!, opts)
+            .then((mongoose) => {
+                return mongoose
+            })
+    }
+
+    cached.conn = await cached.promise
+    
+    return cached.conn
+}
+
+export default dbConnect
