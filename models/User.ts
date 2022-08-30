58b69f2aba91fbdef7f852d302b7c5a7003e7ddb
gegehprast <gegeh.prast@gmail.com>
2022-08-31 02:00:17 +0700

    user model

---
+++ models/User.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface IUserDoc extends Document {
+    username: string
+    password: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export const UserSchema: Schema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: new Date(),
+        },
+        updatedAt: {
+            type: Date,
+            default: new Date(),
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+)
+
+UserSchema.index({ username: -1 }, { unique: true })
+
+const User = mongoose.model<IUserDoc>('User', UserSchema)
+
+export default User
