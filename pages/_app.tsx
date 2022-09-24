f838407b230bea0770df9472f0756e5da548f0b2
gegehprast <gegeh.prast@gmail.com>
2022-09-25 00:32:50 +0700

    getLayout and MainLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,28 @@
 import type { AppProps } from 'next/app'
+import { NextPage } from 'next'
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
+import MainLayout from '../components/layouts/MainLayout'
+import { ReactElement } from 'react'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    return (
-        <SessionProvider session={session}>
+export type MyNextPage<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement<P>) => ReactElement
+}
+
+type ComponentProps = AppProps & {
+    Component?: MyNextPage
+}
+
+function MyApp({
+    Component,
+    pageProps,
+}: ComponentProps) {
+    const getLayout =
+        Component.getLayout ||
+        ((page: ReactElement) => <MainLayout>{page}</MainLayout>)
+
+    return getLayout(
+        <SessionProvider session={pageProps.session}>
             <Component {...pageProps} />
         </SessionProvider>
     )
