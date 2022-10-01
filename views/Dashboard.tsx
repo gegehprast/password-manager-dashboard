d480cb645025749748001da64fd21eac2a0bd31a
gegehprast <gegeh.prast@gmail.com>
2022-10-02 02:59:10 +0700

    idk

--- views/Dashboard.tsx
+++ views/Dashboard.tsx
@@ -1,24 +1,7 @@
 import React from 'react'
-import { signOut } from 'next-auth/react'
 
 const Dashboard = () => {
-    return (
-        <div>
-                <button
-                    className="px-2 py-1 mr-2 text-white bg-blue-600 rounded hover:bg-blue-500"
-                    onClick={() => signOut()}
-                >
-                    Dashboard
-                </button>
-
-                <button
-                    className="px-2 py-1 bg-gray-200 rounded hover:bg-gray-100"
-                    onClick={() => signOut()}
-                >
-                    Sign out
-                </button>
-        </div>
-    )
+    return <div className="w-full"></div>
 }
 
 export default Dashboard
